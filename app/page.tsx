f7ad79de7cdc3f5c84443c4b9a0fd7763c1c4223
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2025-01-13 21:06:47 +0000

    Implement server actions for creating and displaying todos, restructure database connection, and update Next.js configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,57 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  );
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import prisma from "./db";
+import { revalidatePath } from "next/cache";
+
+async function getTodos() {
+	"use server";
+	const todos = await prisma.todo.findMany({
+		select: {
+			id: true,
+			input: true,
+		},
+		orderBy: {
+			createdAt: "desc",
+		},
+	});
+	return todos;
+}
+
+export default async function Home() {
+	const data = await getTodos();
+	async function createTodo(formData: FormData) {
+		"use server";
+		const inputData = formData.get("input") as string;
+		await prisma.todo.create({
+			data: {
+				input: inputData,
+			},
+		});
+		revalidatePath("/");
+	}
+
+	return (
+		<>
+			<div className="h-screen w-screen flex justify-center items-center">
+				<div className="rounded-lg border shadow-xl p-10 w-[30vw]">
+					<form className="flex flex-col" action={createTodo}>
+						<Input type="text" name="input" className="mb-2" />
+						<Button className="bg-slate-900 text-white hover:text-slate-900 hover:border hover:border-slate-900">
+							Submit
+						</Button>
+					</form>
+					{data.map((todo) => (
+						<div
+							key={todo.id}
+							className="flex justify-between items-center border-b py-2">
+							<p>{todo.input}</p>
+							<Button className="bg-red-500 text-white hover:bg-white hover:text-red-500 hover:border hover:border-red-500">
+								Delete
+							</Button>
+						</div>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000"],
+      // allowedForwardedHosts: ["localhost:3000"],
+      // ^ You might have to use this property depending on your exact version.
+    }
+  }
 };
 
 export default nextConfig;
